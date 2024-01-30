bbcf877217d0312a2f98a2f2e63109aa59d5e85d
SissyManson <komandira123@gmail.com>
2024-01-30 16:58:22 +0200

    Added message for no movies

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,7 +5,7 @@ const movieService = require('../services/movieService.js');
 router.get('/', (req, res) => {
    const movies = movieService.getAll();
 
-   res.render('home', { movies });
+   res.render('home', { movies});
 });
 
 router.get('/about', (req, res) => {
