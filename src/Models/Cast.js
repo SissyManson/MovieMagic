49a5bb870e0b7005c29bb4fe9a0b0ce426f59caa
SissyManson <komandira123@gmail.com>
2024-02-02 18:54:42 +0200

    Created cast model

---
+++ src/Models/Cast.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const castSchema = new mongoose.Schema({
+   name: { type: String, required: true },
+   age: { type: Number, required: true, max: 120, min: 14 },
+   born: { type: String, required: true },
+   nameInMovie: { type: String, required: true },
+   castImage: {
+      type: String,
+      required: true,
+      validate: {
+         validator(value) {
+            /^https?:\/\//.test(value);
+         },
+         message: (props) =>
+            `${props.value} is an invalid URL for the castImage`,
+      },
+   }, 
+});
+
+const Cast = mongoose.model('Cast', castSchema);
+
+module.exports = Cast;
