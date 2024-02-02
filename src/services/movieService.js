e939ca11e9c48f72290c108452f634aa312c9ab5
SissyManson <komandira123@gmail.com>
2024-02-02 22:45:57 +0200

    Attached cast to movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,6 +1,5 @@
 const Movie = require('../Models/Movie');
 
-
 // TODO: filter result in MongoDB
 exports.search = async (title, genre, year) => {
    let result = await Movie.find().lean();
@@ -26,3 +25,13 @@ exports.getAll = () => Movie.find();
 //TODO: do sth when there are NO movies
 exports.getMovieById = (movieId) => Movie.findById(movieId);
 exports.create = (movieData) => Movie.create(movieData);
+exports.attach = (movieId, castId) => {
+   // TODO: validate castId if it exists
+   
+   // const movie = await this.getMovieById(movieId);
+   // movie.casts.push(castId);
+   // return movie.save();
+   
+   //TODO: validate if the cast is already added
+   return Movie.findByIdAndUpdate(movieId, { $push: { casts: castId } });
+};
