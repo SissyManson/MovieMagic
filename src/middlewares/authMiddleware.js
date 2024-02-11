a4feed0062b5f5dc45fb6b4d8e6e284e0a195f30
SissyManson <komandira123@gmail.com>
2024-02-11 14:06:35 +0200

    Added logout

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -12,6 +12,8 @@ exports.auth = async (req, res, next) => {
       const decodedToken = await jwt.verify(token, SECRET);
 
       req.user = decodedToken;
+      
+      res.locals.isAuthenticated = true;
 
       next();
    } catch {
@@ -22,8 +24,8 @@ exports.auth = async (req, res, next) => {
 
 exports.isAuth = (req, res, next) => {
    if (!req.user) {
-      res.redirect('/auth/login');
+      return res.redirect('/auth/login');
    }
-   
+
    next();
 };
